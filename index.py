d02c3c257fc2e991621f457cc8df791c18e7b0bc
Nandang <77793855+BegalNenen@users.noreply.github.com>
2022-10-14 20:15:20 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-app = Flask(__name__, template_folder="template")
+app = Flask(__name__, template_folder="template", static_folder="assets")
 
 @app.route("/")
 def index():
