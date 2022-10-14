6c54317c186c9258ee55e0cea613e5148a1e8597
Nandang <77793855+BegalNenen@users.noreply.github.com>
2022-10-14 20:04:00 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+app = Flask(__name__, template_folder="template")
+
+@app.route("/")
+def index():
+	return render_template("/index.html")
+
+@app.errorhandler(404) 
+def invalid_route(e): 
+    return '<meta name="viewport" content="width=device-width,initial-scale=1.0">Cari apa kak?'	
+			
+@app.route("/result",methods=["GET","POST"])
+def result():
+	if request.method == "POST":
+		nama = request.form["author"]
+		kutipan = request.form["quotes"]
+		return render_template("/result.html", author=nama, quote=kutipan)
+	else:
+		return render_template("/index.html")
+
+if __name__ == '__main__':
+	app.run(debug = True)
+	
